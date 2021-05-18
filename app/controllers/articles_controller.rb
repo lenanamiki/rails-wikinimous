db1dc11747ff1f8140f90cb04f5a15198bda5c7c
Lena Namiki <lena.namiki@gmail.com>
2021-05-18 16:37:55 +0900

    Created #new and #create, and new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      @error_message = 'Inputs invalid. Please try again.'
+      render 'new.html.erb'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
